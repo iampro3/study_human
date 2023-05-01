3fde7489f2b89ffcbc0b701eda715ab137eaa82f
iampro3 <lucky1977@daum.net>
2023-05-01 17:25:24 +0900

    0501_DOM.querySelector study

--- 0501_crawling/js/webtoon.js
+++ 0501_crawling/js/webtoon.js
@@ -13,7 +13,10 @@ function getlist(){
     //// 세팅
     // 세팅
     //let url = "https://cors-anywhere.herokuapp.com/https://comic.naver.com/index";
-    let url = "https://cors-anywhere.herokuapp.com/https://comic.naver.com/api/realtime/ranking/list?rankTabType=DEFAULT";
+    //let url = "https://cors-anywhere.herokuapp.com/https://comic.naver.com/api/realtime/ranking/list?rankTabType=DEFAULT";
+
+    let url = "https://zdnet.co.kr/news/?lstcode=0060&page=1";
+    url = "https://api.allorigins.win/get?url="+encodeURIComponent(url);
     xhr.open("get", url);
     
     // 실행
@@ -24,7 +27,47 @@ function getlist(){
     xhr.onload = function(){
         let data = xhr.responseText;
         console.log("------------------------")
-        console.log(data);
+        //console.log(data) // 제이슨을 확인해보자.
+        let json_data = JSON.parse(data);
+        //console.log(json_data).contents;
+        //let html = json_data.contents;        
+        data = json_data.contents;        
+
+        //console.log(html)
+
+        let  domParser = new DOMParser();
+        let html = domParser.parseFromString(data, "text/html");
+
+        //let h3 = html.querySelector("body > div.contentWrapper > div > div.left_cont > div.news_box > div:nth-child(1) > div.assetText > a > h3")
+        // 가져온 내용에서 원하는 것만 선택
+        let list_newsPost = html.querySelectorAll(".newsPost")
+        let result = ""
+
+        for(let i=0; i < list_newsPost.length; i++){
+            let h3 = list_newsPost[i].querySelector(".assetText h3")
+            if(h3){
+                let title = list_newsPost[i].querySelector(".assetText h3").innerHTML
+                let date = list_newsPost[i].querySelector(".byline span").innerHTML
+                let writer = list_newsPost[i].querySelector(".byline a").innerHTML
+
+
+                let img_src = list_newsPost[i].querySelector(".assetThumb img").getAttribute("data-src")
+        
+                result += "<div>"
+                result += "     <h3>"+ title + "</h3>"
+                result += "     <img src="+ img_src +">"
+                result += "     <span>"+ date +"</span>"
+                result += "     <a>"+ writer +"</a>"
+                result += "</div>"
+            }
+        }        
+
+        let list = document.querySelector("#list");
+        list.innerHTML = result;
+
+        //console.log(title_1)
         console.log("------------------------")
     }
+
+
 }
